30f0cdae0d36a2736390588ae8b859af14c2fc99
yuhao1122 <1194752204@qq.com>
2019-01-30 13:28:00 +0800

    update

--- src/main/java/com/lesson/service/impl/CategoryManagerImpl.java
+++ src/main/java/com/lesson/service/impl/CategoryManagerImpl.java
@@ -1,44 +1,45 @@
 package com.lesson.service.impl;
 
-import com.lesson.dao.CategoryDAO;
-import com.lesson.model.Category;
-import com.lesson.service.CategoryManager;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.lesson.dao.CategoryDAO;
+import com.lesson.model.Category;
+import com.lesson.service.CategoryManager;
 
 @Service
 public class CategoryManagerImpl implements CategoryManager {
 
-    Logger logger = Logger.getLogger(CategoryManagerImpl.class);
+	Logger logger = Logger.getLogger(CategoryManagerImpl.class);
 
-    @Autowired
-    CategoryDAO categoryDAO;
+	@Autowired
+	CategoryDAO categoryDAO;
 
-    public List<Category> getAllCategories() {
-        return categoryDAO.getAllCategories();
-    }
+	public List<Category> getAllCategories() {
+		return categoryDAO.getAllCategories();
+	}
 
-    public Category getCategoryById(int cid) {
-        List<Category> categories = categoryDAO.getCategoriesById(cid);
-        if (categories == null || categories.size() == 0 || categories.size() > 1) {
-            return null;
-        }
+	public Category getCategoryById(int cid) {
+		List<Category> categories = categoryDAO.getCategoriesById(cid);
+		if (categories == null || categories.size() == 0 || categories.size() > 1) {
+			return null;
+		}
 
-        return categories.get(0);
-    }
+		return categories.get(0);
+	}
 
-    public int addCategory(String cname) {
-        return categoryDAO.addCategory(cname);
-    }
+	public int addCategory(String cname) {
+		return categoryDAO.addCategory(cname);
+	}
 
-    public int updateCategoryById(int cid, String cname) {
-        return categoryDAO.updateCategoryById(cid, cname);
-    }
+	public int updateCategoryById(int cid, String cname) {
+		return categoryDAO.updateCategoryById(cid, cname);
+	}
 
-    public int deleteCategoryById(int cid) {
-        return categoryDAO.deleteCategoryById(cid);
-    }
+	public int deleteCategoryById(int cid) {
+		return categoryDAO.deleteCategoryById(cid);
+	}
 }
--- src/main/java/com/lesson/service/impl/MenuManagerImpl.java
+++ src/main/java/com/lesson/service/impl/MenuManagerImpl.java
@@ -1,67 +1,68 @@
 package com.lesson.service.impl;
 
-import com.lesson.model.Menu;
-import com.lesson.dao.MenuDAO;
-import com.lesson.service.MenuManager;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.lesson.dao.MenuDAO;
+import com.lesson.model.Menu;
+import com.lesson.service.MenuManager;
 
 @Service
 public class MenuManagerImpl implements MenuManager {
-    Logger logger = Logger.getLogger(MenuManagerImpl.class);
+	Logger logger = Logger.getLogger(MenuManagerImpl.class);
 
-    @Autowired
-    MenuDAO menuDAO;
+	@Autowired
+	MenuDAO menuDAO;
 
-    public List<Menu> getAllMenus() {
-        return menuDAO.getAllMenus();
-    }
+	public List<Menu> getAllMenus() {
+		return menuDAO.getAllMenus();
+	}
 
-    public List<Menu> getMenusByMidCid(String mid, String cid) {
-        return menuDAO.getMenuByMidCid(mid, cid);
-    }
+	public List<Menu> getMenusByMidCid(String mid, String cid) {
+		return menuDAO.getMenuByMidCid(mid, cid);
+	}
 
-    public Menu getMenuByMid(String mid) {
-        List<Menu> menus = menuDAO.getMenuByMidCid(mid, "%");
+	public Menu getMenuByMid(String mid) {
+		List<Menu> menus = menuDAO.getMenuByMidCid(mid, "%");
 
-        if (menus == null) { //如果是空直接返回null
-            logger.info("查询menu无返回接口，请检查后台是否出错！");
-            return null;
-        }
+		if (menus == null) { // 如果是空直接返回null
+			logger.info("查询menu无返回接口，请检查后台是否出错！");
+			return null;
+		}
 
-        int size = menus.size();
+		int size = menus.size();
 
-        if (size == 0) {
-            logger.info("菜品查询返回结果为空 mid = " + mid);
-            return null;
-        } else if (size > 1) {
-            logger.error("DB mid 重复 mid = " + mid);
-            return null;
-        }
+		if (size == 0) {
+			logger.info("菜品查询返回结果为空 mid = " + mid);
+			return null;
+		} else if (size > 1) {
+			logger.error("DB mid 重复 mid = " + mid);
+			return null;
+		}
 
-        return menus.get(0);
-    }
+		return menus.get(0);
+	}
 
-    public int addMenu(int cid, String mname, float price) {
-        logger.info("添加菜品 cid = " + cid + ", mname = " + mname + ", price = " + price);
-        return menuDAO.addMenu(cid, mname, price);
-    }
+	public int addMenu(int cid, String mname, float price) {
+		logger.info("添加菜品 cid = " + cid + ", mname = " + mname + ", price = " + price);
+		return menuDAO.addMenu(cid, mname, price);
+	}
 
-    public int updateMenuByMid(int mid, int cid, String mname, float price) {
-        logger.info("更新菜品详情 mid = " + mid + ", cid = " + cid + ", mname = " + mname + ", price = " + price);
-        return menuDAO.updateMenuByMid(mid, cid, mname, price);
-    }
+	public int updateMenuByMid(int mid, int cid, String mname, float price) {
+		logger.info("更新菜品详情 mid = " + mid + ", cid = " + cid + ", mname = " + mname + ", price = " + price);
+		return menuDAO.updateMenuByMid(mid, cid, mname, price);
+	}
 
-    public int deleteMenuByMid(int mid) {
-        int inpactRowNum = menuDAO.deleteMenuByMid(mid);
-        if (inpactRowNum == 1) {
-            logger.info("对应菜品已被删除，mid = " + mid);
-        } else {
-            logger.info("对应菜品删除失败, mid = " + mid);
-        }
-        return inpactRowNum;
-    }
+	public int deleteMenuByMid(int mid) {
+		int inpactRowNum = menuDAO.deleteMenuByMid(mid);
+		if (inpactRowNum == 1) {
+			logger.info("对应菜品已被删除，mid = " + mid);
+		} else {
+			logger.info("对应菜品删除失败, mid = " + mid);
+		}
+		return inpactRowNum;
+	}
 }
